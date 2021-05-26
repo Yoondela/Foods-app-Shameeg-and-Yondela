47a51b31b9a3c138937d836130fcee5ca9c3a5bd
Shameeg <msalie@student.wethinkcode.co.za>
2021-05-26 10:02:46 +0200

    code cleanup

--- app/src/main/java/com/example/foodapplication/MainActivity.kt
+++ app/src/main/java/com/example/foodapplication/MainActivity.kt
@@ -3,32 +3,24 @@ package com.example.foodapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    lateinit var editTextFood: EditText
-    lateinit var  buttonClickMe: Button
-    lateinit var amount: EditText
-    lateinit var textViewOutput: TextView
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        editTextFood = findViewById(R.id.txtFood)
-        buttonClickMe = findViewById(R.id.btnAdd)
-        textViewOutput = findViewById(R.id.tViewFoods)
-        amount = findViewById(R.id.txtAmount)
-
-        buttonClickMe.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
+    fun onClick(v: View?) {
+
+        var editTextFood: EditText = findViewById(R.id.txtFood)
+        var amount: EditText = findViewById(R.id.txtAmount)
+        var textViewOutput: TextView = findViewById(R.id.tViewFoods)
         val food = editTextFood.text
         val foodAmount = amount.text
+
         textViewOutput.append("$food $foodAmount\n")
     }
 }
