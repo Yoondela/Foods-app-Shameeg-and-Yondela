08507a3ea6569a049ce5b3e823e149baed2f8e3f
Shameeg <msalie@student.wethinkcode.co.za>
2021-05-27 11:58:03 +0200

    Created a hashmap that store mock data. Added logic to get the calories for the type of food entered and created a textView box to display the total amount of calories consumed.

--- app/src/main/java/com/example/foodapplication/MainActivity.kt
+++ app/src/main/java/com/example/foodapplication/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity(){
 
+    var calories = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,21 +17,40 @@ class MainActivity : AppCompatActivity(){
 
     fun onClick(v: View?) {
 
-        var ourListOfFood: HashMap<String, Int> = hashMapOf("apple" to 95, "banana" to 89, "egg" to 155)
+        var mockFoodData: HashMap<String, Int> = hashMapOf("apple" to 95, "banana" to 89, "egg" to 155)
         var editTextFood: EditText = findViewById(R.id.txtFood)
         var amount: EditText = findViewById(R.id.txtAmount)
         var textViewOutput: TextView = findViewById(R.id.tViewFoods)
         val food = editTextFood.text
         val foodAmount = amount.text
-        var calories = 0
 
-        if(food.toString() == "apple"){
-            calories += ourListOfFood["apple"]!!
+        when {
+            food.toString() == "Apple" -> {
+                calories += if(foodAmount.toString()  == ""){
+                    mockFoodData["apple"]!!
+                } else{
+                    mockFoodData["apple"]?.times(foodAmount.toString().toInt())!!
+                }
+            }
+            food.toString() == "Banana" -> {
+                calories += if(foodAmount.toString()  == ""){
+                    mockFoodData["banana"]!!
+                } else{
+                    mockFoodData["banana"]?.times(foodAmount.toString().toInt())!!
+                }
+            }
+            food.toString() == "Egg" -> {
+                calories += if(foodAmount.toString()  == ""){
+                    mockFoodData["egg"]!!
+                } else{
+                    mockFoodData["egg"]?.times(foodAmount.toString().toInt())!!
+                }
+            }
         }
 
         var textViewCalories : TextView = findViewById(R.id.textView2)
 
-        textViewCalories.append(calories.toString())
+        textViewCalories.text = calories.toString()
         textViewOutput.append("$food $foodAmount\n")
     }
 }
