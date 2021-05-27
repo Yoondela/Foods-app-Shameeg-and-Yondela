1ebb1b610eaa4d0296cd81805f885139123f7950
Shameeg <msalie@student.wethinkcode.co.za>
2021-05-27 11:02:05 +0200

    added basic logic to check calories based on user input

--- app/src/main/java/com/example/foodapplication/MainActivity.kt
+++ app/src/main/java/com/example/foodapplication/MainActivity.kt
@@ -15,12 +15,21 @@ class MainActivity : AppCompatActivity(){
 
     fun onClick(v: View?) {
 
+        var ourListOfFood: HashMap<String, Int> = hashMapOf("apple" to 95, "banana" to 89, "egg" to 155)
         var editTextFood: EditText = findViewById(R.id.txtFood)
         var amount: EditText = findViewById(R.id.txtAmount)
         var textViewOutput: TextView = findViewById(R.id.tViewFoods)
         val food = editTextFood.text
         val foodAmount = amount.text
+        var calories = 0
 
+        if(food.toString() == "apple"){
+            calories += ourListOfFood["apple"]!!
+        }
+
+        var textViewCalories : TextView = findViewById(R.id.textView2)
+
+        textViewCalories.append(calories.toString())
         textViewOutput.append("$food $foodAmount\n")
     }
 }
