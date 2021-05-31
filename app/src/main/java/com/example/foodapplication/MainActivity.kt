fefe63ded0abc3a5152e79aac8a1149c2226148f
Shameeg <msalie@student.wethinkcode.co.za>
2021-05-31 10:10:40 +0200

    I implemented the use of fragments. Switches from a welcome screen to input screen.

--- app/src/main/java/com/example/foodapplication/MainActivity.kt
+++ app/src/main/java/com/example/foodapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.foodapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
@@ -13,50 +14,78 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        goNextPage()
+
     }
 
-    fun onClick(v: View?) {
+    fun goNextPage(){
 
-        var mockFoodData: HashMap<String, Int> = hashMapOf("apple" to 95, "banana" to 89, "egg" to 155)
-        var editTextFood: EditText = findViewById(R.id.txtFood)
-        var amount: EditText = findViewById(R.id.txtAmount)
-        var textViewOutput: TextView = findViewById(R.id.tViewFoods)
-        val food = editTextFood.text
-        val foodAmount = amount.text
+        val welcomeFragment = WelcomeFragment()
+        val inputFragment = InputFragment()
+        val btContinue : Button = findViewById(R.id.btnContinue)
 
-        when {
-            food.toString() == "Apple" -> {
-                calories += if(foodAmount.toString()  == ""){
-                    mockFoodData["apple"]!!
-                } else{
-                    mockFoodData["apple"]?.times(foodAmount.toString().toInt())!!
-                }
-            }
-            food.toString() == "Banana" -> {
-                calories += if(foodAmount.toString()  == ""){
-                    mockFoodData["banana"]!!
-                } else{
-                    mockFoodData["banana"]?.times(foodAmount.toString().toInt())!!
-                }
-            }
-            food.toString() == "Egg" -> {
-                calories += if(foodAmount.toString()  == ""){
-                    mockFoodData["egg"]!!
-                } else{
-                    mockFoodData["egg"]?.times(foodAmount.toString().toInt())!!
-                }
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, welcomeFragment)
+            commit()
+        }
+
+        btContinue.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, inputFragment)
+                commit()
+                addToBackStack(null)
             }
         }
 
-        var textViewCalories : TextView = findViewById(R.id.textView2)
 
-        textViewCalories.text = calories.toString()
-        textViewOutput.append("$food $foodAmount\n")
     }
 
-    fun clearAll(v: View){
-
-        var textViewOutput: TextView = findViewById(R.id.tViewFoods)
-        textViewOutput.text = ""
-    }
+//    fun onClick(v: View?) {
+//
+//        var mockFoodData: HashMap<String, Int> = hashMapOf("apple" to 95, "banana" to 89, "egg" to 155)
+//        var editTextFood: EditText = findViewById(R.id.txtFood)
+//        var amount: EditText = findViewById(R.id.txtAmount)
+//        var textViewOutput: TextView = findViewById(R.id.tViewFoods)
+//        val food = editTextFood.text
+//        val foodAmount = amount.text
+//
+//        when {
+//            food.toString() == "Apple" -> {
+//                calories += if(foodAmount.toString()  == ""){
+//                    mockFoodData["apple"]!!
+//                } else{
+//                    mockFoodData["apple"]?.times(foodAmount.toString().toInt())!!
+//                }
+//            }
+//            food.toString() == "Banana" -> {
+//                calories += if(foodAmount.toString()  == ""){
+//                    mockFoodData["banana"]!!
+//                } else{
+//                    mockFoodData["banana"]?.times(foodAmount.toString().toInt())!!
+//                }
+//            }
+//            food.toString() == "Egg" -> {
+//                calories += if(foodAmount.toString()  == ""){
+//                    mockFoodData["egg"]!!
+//                } else{
+//                    mockFoodData["egg"]?.times(foodAmount.toString().toInt())!!
+//                }
+//            }
+//        }
+//
+//        var textViewCalories : TextView = findViewById(R.id.textView2)
+//
+//        textViewCalories.text = calories.toString()
+//        textViewOutput.append("$food $foodAmount\n")
+//    }
+//
+//    fun clearAll(v: View){
+//
+//        var textViewOutput: TextView = findViewById(R.id.tViewFoods)
+//        var textViewCalories : TextView = findViewById(R.id.textView2)
+//        calories = 0
+//        textViewCalories.text = calories.toString()
+//        textViewOutput.text = ""
+//    }
 }
