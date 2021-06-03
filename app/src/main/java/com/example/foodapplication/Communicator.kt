532f98286104538a64aab440113a6cf63fd24a12
Shameeg <msalie@student.wethinkcode.co.za>
2021-06-03 16:28:56 +0200

    fixed a problem where if we changed orientation it would go back to the home screen. Also did a bit of a code clean up.

--- app/src/main/java/com/example/foodapplication/Communicator.kt
+++ app/src/main/java/com/example/foodapplication/Communicator.kt
@@ -2,4 +2,8 @@ package com.example.foodapplication
 
 interface Communicator {
     fun passData(calories: String)
+
+    fun checkCals():Int
+
+    fun resetCals():Int
 }
--- app/src/main/java/com/example/foodapplication/MainActivity.kt
+++ app/src/main/java/com/example/foodapplication/MainActivity.kt
@@ -2,14 +2,19 @@ package com.example.foodapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity(), Communicator {
+
+    var calories = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setDefaultScreen()
+        if (savedInstanceState==null){
+            setDefaultScreen()
+        }
     }
 
     private fun setDefaultScreen() {
@@ -33,5 +38,47 @@ class MainActivity : AppCompatActivity(), Communicator {
         transaction.replace(R.id.mainLayout, outputFragment)
 //        transaction.addToBackStack(null)
         transaction.commit()
+    }
+
+    override fun checkCals():Int{
+
+
+        val mockFoodData: HashMap<String, Int> =
+            hashMapOf("apple" to 95, "banana" to 89, "egg" to 155)
+        val editTextFood: EditText = findViewById(R.id.txtFood)
+        val amount: EditText = findViewById(R.id.txtAmount)
+        val food = editTextFood.text
+        val foodAmount = amount.text
+
+        when {
+            food.toString() == "Apple" -> {
+                calories += if (foodAmount.toString() == "") {
+                    mockFoodData["apple"]!!
+                } else {
+                    mockFoodData["apple"]?.times(foodAmount.toString().toInt())!!
+                }
+            }
+            food.toString() == "Banana" -> {
+                calories += if (foodAmount.toString() == "") {
+                    mockFoodData["banana"]!!
+                } else {
+                    mockFoodData["banana"]?.times(foodAmount.toString().toInt())!!
+                }
+            }
+            food.toString() == "Egg" -> {
+                calories += if (foodAmount.toString() == "") {
+                    mockFoodData["egg"]!!
+                } else {
+                    mockFoodData["egg"]?.times(foodAmount.toString().toInt())!!
+                }
+            }
+        }
+        return calories
+    }
+
+    override fun resetCals():Int{
+        calories = 0
+
+        return calories
     }
 }
