4b8ad14c890765162d19e27c0e0d931e439a4d78
Shameeg <msalie@student.wethinkcode.co.za>
2021-06-11 18:08:04 +0200

    Added code to add all items added by user to a stringBuilder which then makes one large call to the api and calories gets updated accordingly.refactored the code a bit so that we get all the information back from the api before switching to a new screen.

--- app/src/main/java/com/example/foodapplication/Communicator.kt
+++ app/src/main/java/com/example/foodapplication/Communicator.kt
@@ -1,18 +1,13 @@
 package com.example.foodapplication
 
-import okhttp3.Response
-import org.json.JSONObject
-
 interface Communicator {
 
     fun passData(calories: String)
 
-    // we'll have to make changes to this function to update the calories according to what we get back from api
-    fun checkCals(response: Response)
+    fun resetCals():Double
 
-    fun resetCals():String
+    fun getJson():Double
 
-    //a function that gets the json object and returns the JSONObject
-    fun getJson():String
+    fun getFoodList():ArrayList<String>
 
 }
--- app/src/main/java/com/example/foodapplication/InputFragment.kt
+++ app/src/main/java/com/example/foodapplication/InputFragment.kt
@@ -7,26 +7,25 @@ import android.widget.*
 
 class InputFragment : Fragment(R.layout.fragment_input){
 
-    private var calories = ""
+    private var calories = 0.0
     var arrayList = ArrayList<String>()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val root = inflater.inflate(R.layout.fragment_input, container, false)
         var communicator: Communicator = activity as Communicator
-        val editTextFood: EditText = root.findViewById(R.id.txtFood)
-        val amount: EditText = root.findViewById(R.id.txtAmount)
         val btnAdd: Button = root.findViewById(R.id.btnAdd)
         val btnClear: Button = root.findViewById(R.id.btnClear)
         val btnContinue = root.findViewById<Button>(R.id.btnCont)
         val list: ListView = root.findViewById(R.id.list)
-//        val arrayList = ArrayList<String>()
         val myAdapter = ArrayAdapter(requireActivity(), android.R.layout.simple_list_item_1, arrayList)
         list.adapter = myAdapter
 
         btnAdd.setOnClickListener {
-//            communicator.getJson()
-            calories = communicator.getJson()
-            arrayList.add(editTextFood.text.toString() + " " +  amount.text.toString())
+            for (i in communicator.getFoodList()){
+                if (!arrayList.contains(i)){
+                    arrayList.add(i)
+                }
+            }
             myAdapter.notifyDataSetChanged()
         }
 
@@ -37,11 +36,9 @@ class InputFragment : Fragment(R.layout.fragment_input){
         }
 
         btnContinue.setOnClickListener {
-            communicator.passData(calories)
+            calories = communicator.getJson()
         }
 
         return root
     }
-
-
 }
