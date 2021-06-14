d29fce1d38cc3b03da521d053669177d1ec98c0b
Shameeg <msalie@student.wethinkcode.co.za>
2021-06-14 10:24:51 +0200

    Refactored code. back button is now working as expected.

--- app/src/main/java/com/example/foodapplication/MainActivity.kt
+++ app/src/main/java/com/example/foodapplication/MainActivity.kt
@@ -3,16 +3,8 @@ package com.example.foodapplication
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import com.google.gson.GsonBuilder
-import okhttp3.*
-import java.io.IOException
 
 class MainActivity : AppCompatActivity(), Communicator {
-
-    private var calories = 0.0
-    var listOfFood = ArrayList<String>()
-    var query = StringBuilder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,61 +35,8 @@ class MainActivity : AppCompatActivity(), Communicator {
 
         outputFragment.arguments = bundle
         transaction.replace(R.id.mainLayout, outputFragment)
-//        transaction.addToBackStack(null)
+        transaction.addToBackStack(null)
         transaction.commit()
-    }
-
-    override fun resetCals(){
-        query.clear()
-        listOfFood.clear()
-    }
-
-    override fun getJson():Double{
-
-        for (i in listOfFood){
-            query.append("$i ")
-        }
-
-        val client = OkHttpClient()
-        val url = "https://calorieninjas.p.rapidapi.com/v1/nutrition?query= $query"
-        val request = Request.Builder()
-            .url(url)
-            .get()
-            .addHeader("x-rapidapi-key", "d2e7ffc1f5mshfeab8c7bb8fb7d4p1c7c36jsnb23dcbe86dae")
-            .addHeader("x-rapidapi-host", "calorieninjas.p.rapidapi.com")
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                if(response.isSuccessful){
-
-                    val jsonResponse = response.body()?.string()
-                    val gson = GsonBuilder().create()
-                    val data = gson.fromJson(jsonResponse, Data::class.java)
-
-                    for (element in data.items){
-                        calories += element.calories
-                    }
-                    passData(calories.toString())
-                }
-            }
-        })
-
-        return calories
-    }
-
-    override fun getFoodList():ArrayList<String>{
-
-        val etFood = findViewById<EditText>(R.id.txtFood)
-
-        listOfFood.add(etFood.text.toString())
-
-        return listOfFood
     }
 }
 
