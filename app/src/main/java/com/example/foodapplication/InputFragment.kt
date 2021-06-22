d9db4471e99f21dd1c6e6e85f4c24d5b6286ce4f
Shameeg <msalie@student.wethinkcode.co.za>
2021-06-22 10:51:40 +0200

    refactored code and removed 'food amount' input field.

--- app/src/main/java/com/example/foodapplication/InputFragment.kt
+++ app/src/main/java/com/example/foodapplication/InputFragment.kt
@@ -9,24 +9,83 @@ import okhttp3.*
 import java.io.IOException
 import java.lang.StringBuilder
 
-class InputFragment : Fragment(R.layout.fragment_input){
+class InputFragment : Fragment(R.layout.fragment_input), Callback {
 
-    private var calories = ""
+    private var calories = 0.0
     private var query = StringBuilder()
     private var listOfFood = ArrayList<String>()
-    var listOfCalories = ArrayList<String>()
+    private var listOfCalories = ArrayList<Double>()
+    private val client = OkHttpClient()
+    inner class Data(val items: List<Items>)
+    inner class Items(val calories: Double)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_input, container, false)
 
-        val root = inflater.inflate(R.layout.fragment_input, container, false)
-        val communicator: Communicator = activity as Communicator
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onClickAllButtons()
+    }
+
+    private fun makeNetworkCall() {
+
+        val url = "https://calorieninjas.p.rapidapi.com/v1/nutrition?query= $query"
+        val request = Request.Builder()
+            .url(url)
+            .get()
+            .addHeader("x-rapidapi-key", "d2e7ffc1f5mshfeab8c7bb8fb7d4p1c7c36jsnb23dcbe86dae")
+            .addHeader("x-rapidapi-host", "calorieninjas.p.rapidapi.com")
+            .build()
+
+        client.newCall(request).enqueue(this)
+    }
+
+    override fun onFailure(call: Call, e: IOException) {
+        e.printStackTrace()
+    }
+
+    override fun onResponse(call: Call, response: Response) {
+        if (response.isSuccessful) {
+
+            val jsonResponse = response.body()?.string()
+            val gson = GsonBuilder().create()
+            val data = gson.fromJson(jsonResponse, Data::class.java)
+
+            for (element in data.items) {
+                listOfCalories.add(element.calories)
+            }
+            query.clear()
+            passData()
+
+        }
+    }
+
+    private fun passData() {
+        val bundle = Bundle()
+        bundle.putDoubleArray("calories", listOfCalories.toDoubleArray())
+
+        val transaction = requireActivity().supportFragmentManager.beginTransaction()
+        val outputFragment = OutputFragment()
+
+        outputFragment.arguments = bundle
+        transaction.replace(R.id.mainLayout, outputFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    private fun onClickAllButtons() {
+
+        val root = requireView()
         val etFood: EditText = root.findViewById(R.id.txtFood)
         val btnAdd: Button = root.findViewById(R.id.btnAdd)
         val btnClear: Button = root.findViewById(R.id.btnClear)
         val btnContinue = root.findViewById<Button>(R.id.btnCont)
-        val list: ListView = root.findViewById(R.id.list)
-        val progressBar:ProgressBar = root.findViewById(R.id.progressBar)
+        val progressBar: ProgressBar = root.findViewById(R.id.progressBar)
         val tvProcessing: TextView = root.findViewById(R.id.process)
+        val list: ListView = root.findViewById(R.id.list)
         val myAdapter = ArrayAdapter(requireActivity(), R.layout.black_text_list, listOfFood)
         list.adapter = myAdapter
 
@@ -40,7 +99,7 @@ class InputFragment : Fragment(R.layout.fragment_input){
 
         btnClear.setOnClickListener {
 
-            calories = ""
+            calories = 0.0
             query.clear()
             listOfFood.clear()
             listOfCalories.clear()
@@ -51,42 +110,7 @@ class InputFragment : Fragment(R.layout.fragment_input){
 
             progressBar.visibility = View.VISIBLE
             tvProcessing.visibility = View.VISIBLE
-
-
-            val client = OkHttpClient()
-            val url = "https://calorieninjas.p.rapidapi.com/v1/nutrition?query= $query"
-            val request = Request.Builder()
-                .url(url)
-                .get()
-                .addHeader("x-rapidapi-key", "d2e7ffc1f5mshfeab8c7bb8fb7d4p1c7c36jsnb23dcbe86dae")
-                .addHeader("x-rapidapi-host", "calorieninjas.p.rapidapi.com")
-                .build()
-
-            client.newCall(request).enqueue(object : Callback {
-
-                override fun onFailure(call: Call, e: IOException) {
-                    e.printStackTrace()
-                }
-
-                override fun onResponse(call: Call, response: Response) {
-                    if(response.isSuccessful){
-
-                        val jsonResponse = response.body()?.string()
-                        val gson = GsonBuilder().create()
-                        val data = gson.fromJson(jsonResponse, Data::class.java)
-
-                        for (element in data.items){
-                            listOfCalories.add(element.calories)
-                        }
-                        query.clear()
-                        println(query)
-                        communicator.passData(listOfCalories)
-
-                    }
-                }
-            })
+            makeNetworkCall()
         }
-
-        return root
     }
 }
