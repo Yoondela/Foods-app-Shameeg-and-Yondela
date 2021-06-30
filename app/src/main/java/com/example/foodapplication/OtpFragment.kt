08cd0045356da27a8215c817d92e259cf9ed95de
Shameeg <msalie@student.wethinkcode.co.za>
2021-06-30 15:55:59 +0200

    fixed the OTP layout.

--- app/src/main/java/com/example/foodapplication/OtpFragment.kt
+++ app/src/main/java/com/example/foodapplication/OtpFragment.kt
@@ -2,6 +2,8 @@ package com.example.foodapplication
 
 import DatabaseHandler
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +12,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
-class OtpFragment : Fragment() {
+class OtpFragment : Fragment(),TextWatcher {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?=  inflater.inflate(R.layout.fragment_otp, container, false)
 
@@ -18,6 +20,14 @@ class OtpFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val root = requireView()
         val submit = root.findViewById<Button>(R.id.submitOTP)
+        val etOTP1 = root.findViewById<EditText>(R.id.etOTP1)
+        val etOTP2 = root.findViewById<EditText>(R.id.etOTP2)
+        val etOTP3 = root.findViewById<EditText>(R.id.etOTP3)
+        val etOTP4 = root.findViewById<EditText>(R.id.etOTP4)
+        etOTP1.addTextChangedListener(this)
+        etOTP2.addTextChangedListener(this)
+        etOTP3.addTextChangedListener(this)
+        etOTP4.addTextChangedListener(this)
 
         submit.setOnClickListener {
             checkOTPAndLogin()
@@ -26,8 +36,11 @@ class OtpFragment : Fragment() {
 
     private fun checkOTPAndLogin(){
         val root = requireView()
-        val inputOTP = root.findViewById<EditText>(R.id.etOTP)
-        val OTP = inputOTP.text.toString()
+        val etOTP1 = root.findViewById<EditText>(R.id.etOTP1)
+        val etOTP2 = root.findViewById<EditText>(R.id.etOTP2)
+        val etOTP3 = root.findViewById<EditText>(R.id.etOTP3)
+        val etOTP4 = root.findViewById<EditText>(R.id.etOTP4)
+        val OTP = "${etOTP1.text}${etOTP2.text}${etOTP3.text}${etOTP4.text}"
         val dbHandler = DatabaseHandler(requireContext())
         val user = User(OTP)
 
@@ -46,5 +59,34 @@ class OtpFragment : Fragment() {
         val transaction = requireActivity().supportFragmentManager.beginTransaction()
         transaction.replace(R.id.mainLayout, loginFragment)
         transaction.commit()
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        checkIfFieldIsFull()
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+    }
+
+    private fun checkIfFieldIsFull() {
+
+        val root = requireView()
+        val etOTP1 = root.findViewById<EditText>(R.id.etOTP1)
+        val etOTP2 = root.findViewById<EditText>(R.id.etOTP2)
+        val etOTP3 = root.findViewById<EditText>(R.id.etOTP3)
+        val etOTP4 = root.findViewById<EditText>(R.id.etOTP4)
+
+        if (etOTP1.text.isNotEmpty()) {
+            etOTP2.requestFocus()
+        }
+        if (etOTP2.text.isNotEmpty()) {
+            etOTP3.requestFocus()
+        }
+        if (etOTP3.text.isNotEmpty()) {
+            etOTP4.requestFocus()
+        }
     }
 }
